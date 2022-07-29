a98ebf62db5de21f42b612b6b0d3d9fc8780c3c4
selvakumarsk028 <110168961+selvakumarsk028@users.noreply.github.com>
2022-07-29 11:54:45 +0530

    Add files via upload

---
+++ S.java
@@ -0,0 +1,24 @@
+class S
+{
+     public static void main(String[]args)
+     {
+		 //print name
+		 System.out.println("core java");
+		 
+		 System.out.println("java is progaram language 123");
+		 
+		 System.out.println("123456");
+		 
+		 System.out.println("10+20");
+		 
+		 System.out.println("98[][905653212<>]?;{}");
+		 
+		 System.out.println(1020132423);
+	     System.out.println(true);//boolean
+		 System.out.println(false);//boolean
+		 System.out.println(10.20f);//floating
+         System.out.println(0b0011);//integer
+         System.out.println(0x01234abcd);//integer
+         System.out.println(0123456);//integer
+	 }		 
+}
